a39d1a018e97259623c8d325d7ed4895a674fe2d
Srdjan Hinic <srdjan.hinic.3024@metropolitan.ac.rs>
2022-07-23 23:54:33 +0200

    first

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const Discord = require("discord.js");
 const ytdl = require("ytdl-core");
 const yts = require("yt-search");
 const ytpl = require('ytpl');
+const colors = require('colors');
+const fs = require('fs');
+const pbar = require('string-progressbar');
 
 const client = new Discord.Client();
 const serverMap = new Map(); // map of all servers and their queues
+
 
 client.once("ready", () => {
     console.log("Ready!");
@@ -21,7 +25,7 @@ client.once("disconnect", () => {
 
 client.on("message", async message => {
     if (message.author.bot || message.author.id == "342186053970427904" /*bjuko id*/ ) return; //ignore own messages, and bjuko
-    if (!message.content.startsWith(prefix)) return; //ignore messages that dont start with the prefix
+    if (!message.content.startsWith(prefix)) return; //ignore messages that dont start with the prefix 
     const serverQueue = serverMap.get(message.guild.id); //check if this server already has a queue
 
     if (message.content.startsWith(`${prefix}play`) || message.content.startsWith(`${prefix}PLAY`) || message.content.startsWith(`${prefix}p`) || message.content.startsWith(`${prefix}P`)) { //play song
@@ -44,7 +48,7 @@ client.on("message", async message => {
         }
     }
     // clear queue and disconnect
-    else if (message.content.startsWith(`${prefix}odjebi`)) { // clear queue and disconnect
+    else if (message.content.startsWith(`${prefix}reboot`)) { // clear queue and disconnect
         serverQueue.isLooping = false;
         serverMap.delete(message.guild.id);
         message.channel.send("POZ BURAAAAZ");
@@ -60,17 +64,35 @@ client.on("message", async message => {
             try {
                 const embed = new Discord.MessageEmbed(); // display queue as embed (scuffed) - TODO: MAKE IT PRETTIER
                 embed.addField("Songs in the queue:", "_");
-                // embed.fields[0] = "Songs in the queue:", " ";
-                let i = 1;
+                let i = 0;
                 serverQueue.songs.forEach(song => {
-                    embed.addField(i++, `[${song.title}](${song.url})`);
+                    embed.addField(++i, `[${song.title}](${song.url})`);
                 });
                 serverQueue.textChannel.send(embed);
                 return;
             } catch (e) {
                 console.log(e);
-                // message.channel.send(e.error);
             }
+        }
+
+    } else if (message.content.startsWith(`${prefix}qremove`)) {
+        if (!serverQueue) {
+            return message.channel.send("There are no songs in the queue!");
+        }
+        const args = message.content.split(" ");
+        try {
+            index = parseInt(args[1]) - 1;
+            userIndex = index + 1;
+        } catch (e) {
+            console.error();
+            return message.channel.send("Queue remove failed. You must pass the number of the song in the playlist! Type **!queue** to see the queue.")
+        }
+        queueLength = serverQueue.songs.length;
+        if (index > queueLength) {
+            return message.channel.send("There is no song at index " + `${userIndex}` + ".");
+        } else {
+            message.channel.send(`${serverQueue.songs.title}` + " has been removed from the queue.");
+            serverQueue.songs.splice(index, 1);
         }
 
     } else if (message.content.startsWith(`${prefix}clearqueue`)) {
@@ -89,6 +111,7 @@ client.on("message", async message => {
         try { //try to embed now playing on discord
             const embed = new Discord.MessageEmbed();
             embed.addField("Now playing: ", `[${serverQueue.songs[0].title}](${serverQueue.songs[0].url})`);
+            pbar.splitBar
             serverQueue.textChannel.send(embed);
         } catch (e) {
             console.log(e);
@@ -101,7 +124,7 @@ client.on("message", async message => {
     } else if (message.content.startsWith(`${prefix}commands`)) {
         message.channel.send(
             new Discord.MessageEmbed()
-            .addFields({ name: "**!play**", value: "Plays a song from youtube or playlist from youtube. Valid arguments are keywords, direct links or playlist links" }, { name: "**!stop**", value: "Stops the currently playing song, clears the queue, leaves the channel" }, { name: "**!skip**", value: "Skips a song" }, { name: "**!queue**", value: "Displays the queue" }, { name: "**!clearqueue**", value: "Clears the queue" }, { name: "**!odjebi**", value: "Stops everything, clears everything, a reboot switch pretty much" })
+            .addFields({ name: "**!play**", value: "Plays a song from youtube or playlist from youtube. Valid arguments are keywords, direct links or playlist links" }, { name: "**!stop**", value: "Stops the currently playing song, clears the queue, leaves the channel" }, { name: "**!skip**", value: "Skips a song" }, { name: "**!queue**", value: "Displays the queue" }, { name: "**!clearqueue**", value: "Clears the queue" }, { name: "**!reboot**", value: "Stops everything, clears everything, a reboot switch pretty much" })
         );
     } else {
         message.channel.send("You need to enter a valid command, type !commands to get a list of commands.");
@@ -146,15 +169,22 @@ async function execute(message, serverQueue) {
         console.log("Wasn't a playlist!".red);
     }
 
-    if (!isPlaylist) { //if ytpl didn't return a playlist ID it's either a youtube URL or a keyword search
-        try { //figure out if URL or keyword search
-            if (ytdl.validateURL(args[1])) { // ytdl url validation
+    if (!isPlaylist) {
+
+        ///////////////////////////////////////////////////////////////////////////////////////////
+        //  if ytpl didn't return a playlist ID it's either a youtube URL or a keyword search    //
+        //  figure out if URL or keyword search                                                  //
+        ///////////////////////////////////////////////////////////////////////////////////////////
+        try {
+            if (ytdl.validateURL(args[1])) { // YTDL url validation
                 const result = await ytdl.getInfo(args[1]);
                 song = {
                     title: result.videoDetails.title,
                     url: result.videoDetails.video_url
                 };
-            } else { // youtube keyword search
+                console.log("Found song by URL:\n" + "Title: ".green + result.videoDetails.title + "\nURL: ".green + result.videoDetails.video_url);
+                fs.appendFileSync("log.txt", "\n\rFound song by URL:\nTitle: " + result.videoDetails.title + "\nURL: " + result.videoDetails.video_url + "\nRequested by: " + `${message.author.tag}`);
+            } else { // YTS youtube keyword search
                 const { videos } = await yts(args.slice(1).join(" "));
                 if (!videos.length) return message.channel.send("No songs were found!");
                 song = {
@@ -162,9 +192,12 @@ async function execute(message, serverQueue) {
                     url: videos[0].url,
                     type: 1
                 };
+                console.log("Found song by YTS:\n" + "Title: ".green + videos[0].title + "\nURL: ".green + videos[0].url);
+                fs.appendFileSync("log.txt", "\n\rFound song by YTS:\n" + "Title: " + videos[0].title + "\nURL: " + videos[0].url + "\nRequested by: " + `${message.author.tag}`);
             }
-        } catch (e) { // YTDL catch error
-            console.log(e);
+        } catch (e) { // YTDL/YTS catch error
+            console.error();
+            fs.appendFileSync("log.txt", e);
             return;
         }
     }
@@ -178,7 +211,8 @@ async function execute(message, serverQueue) {
             volume: 5,
             playing: true,
             timeOut: null,
-            isLooping: false
+            isLooping: false,
+            currentlyPlayingSongStart: null
 
         };
 
@@ -206,7 +240,8 @@ async function execute(message, serverQueue) {
             queueContruct.connection = connection;
             play(message.guild, queueContruct.songs[0]);
         } catch (e) {
-            console.log(e);
+            console.error();
+            fs.appendFileSync("log.txt", e);
             serverMap.delete(message.guild.id);
             return;
 
@@ -281,19 +316,20 @@ function play(guild, song) { // recursively play songs from serverQueue
             serverQueue.timeOut = null;
         }
         const dispatcher = serverQueue.connection
-            .play(ytdl(song.url, { filter: 'audioonly', highWaterMark: 1 << 25, quality: 'highestaudio' }))
+            .play(ytdl(song.url, { filter: 'audioonly', /*highWaterMark: 1 << 25,*/ quality: 'highestaudio' }))
             .on("finish", () => {
                 if (serverQueue.isLooping == true) {
                     serverQueue.songs.push(serverQueue.songs.shift());
                 } else {
                     serverQueue.songs.shift();
                 }
-                //console.log("Finished: " + `${song.title}` + " URL: " + ` ${song.url}`); //debuf info
+                console.log("Finished playing song " + "\nTitle:".green + `${song.title}` + "\nURL: ".green + ` ${song.url}`); //debuf info
                 play(guild, serverQueue.songs[0]);
+
             })
             .on("error", error => console.error(error));
         dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
-        //console.log("Playing: ".green + `${song.title}` + " URL: ".green + `${song.url}`); //debuf info
+        console.log("Playing" + "\nTitle: ".green + `${song.title}` + "\nURL: ".green + `${song.url}`); //debuf info
 
     }
 }
